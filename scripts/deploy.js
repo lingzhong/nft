83b1262013d878fd207559a9620d219795f63dd8
lingzhong <ling.j.zhong@gmail.com>
2022-03-02 06:13:30 -0800

    Add LeanNFT base

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,14 @@
-async function main() {
-  const EthereumOrgTutorialNFT = await ethers.getContractFactory("EthereumOrgTutorialNFT")
+async function main(nftName) {
+  const NFT = await ethers.getContractFactory(nftName)
 
   // Start deployment, returning a promise that resolves to a contract object
-  const nftContract = await EthereumOrgTutorialNFT.deploy()
+  const nftContract = await NFT.deploy()
   await nftContract.deployed()
   console.log("Contract deployed to address:", nftContract.address)
   console.log("View on Etherscan: https://ropsten.etherscan.io/address/".concat(nftContract.address))
 }
 
-main()
+main("LeanNFT")
   .then(() => process.exit(0))
   .catch((error) => {
     console.error(error)
